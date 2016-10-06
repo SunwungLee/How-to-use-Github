f0b5947b768cd88c4227e7622cf51ccab0db5a98
Sunwung Lee <taebee93@gmail.com>
2016-10-06 00:44:50 -0700

    1st bug123 branch

--- hello.c
+++ hello.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+void foo1()
+{
+	printf("1st corr another branch");
+	return;
+}
+
 void main(void)
 {
 	printf("git test\n");
 	printf("3rd correction 1,2 erased");
+	foo();
 	return;
 }
